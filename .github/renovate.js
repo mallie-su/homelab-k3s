81de2199b9a43a2c7957c5dff8c7af13c02681cb
Matthieu ALLIE <allie.matthieu@gmail.com>
2024-09-15 22:17:55 +0200

    ci: renovate configuration

---
+++ .github/renovate.js
@@ -0,0 +1,8 @@
+module.exports = {
+    branchPrefix: 'chore/',
+    username: 'renovate',
+    gitAuthor: 'Renovate Bot <renovate@sp4rck.ovh>',
+    onboarding: true,
+    platform: 'github',
+    forkProcessing: 'false',
+  };
